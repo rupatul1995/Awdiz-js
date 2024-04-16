0fedbf168fdd3827f5600269e7589ce7b4b8c06c
rupatul1995 <rupalichavan578@gmail.com>
2024-04-17 00:10:30 +0530

    update index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+1. Data 
+
+ data is information of anything -Rutuja ,Awdiz
+
+ 2.Datatype
+
+ name - Rutuja - String
+ age  - 23     -  number
+ yes/no        -Boolean
+ confused      -undefined
+
+
+
+ Rutuja
+ 654651
+ hellow
+ welcome
+ log out
+
+  1.String
+   syntax-rule
+   "" ,'',``
+
+   kuchbhi
+   "kuchbhi"=string
+   'log out'=string
+   `welcom`=string
+
+2. Number
+
+syntax
+12
+486
+54687
+768464
+
+
+3.Boolean
+
+syntax
+
+true false
+
+4.undefined
+
+syntax= pending
+
+varibles
+Storage
+
+
+console.log(myStudentName)
+
+console.log(myStudentAge)
+
+console.log(myStudentCompletedAssignment)
