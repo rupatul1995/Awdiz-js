4a6b93adb9e336cc6c1c879bea09500d35595e8e
rupatul1995 <rupalichavan578@gmail.com>
2024-04-18 15:36:07 +0530

    update for day-2

---
+++ DAY-2/index.js
@@ -0,0 +1,62 @@
+// var mytotalsister = 12;
+// var mytotalbrother = 15;
+// console.log(mytotalsister + mytotalbrother);
+// console.log(mytotalsister - mytotalbrother);
+// console.log(mytotalsister / mytotalbrother);
+// console.log(mytotalsister * mytotalbrother);
+
+// console.log(1, 2, typeof(1))
+// console.log("hello", typeof("hello"))
+// console.log('hello', typeof('hello'))
+// console.log(`hello`, typeof(`hello`))
+// console.log(true, typeof(true))
+// console.log(false, typeof(false))
+
+// console.log(typeof("Institute"))
+// console.log(typeof(true))
+// console.log(typeof(23))
+// console.log(typeof(`false`))
+// console.log(typeof('543'))
+
+// var myName = "Awdiz";
+// console.log(myName , typeof(myName));
+
+
+// undefined
+// var todaysStudent
+// console.log(todaysStudent , typeof(todaysStudent))
+
+
+// declare
+console.log(myClass)
+var myClass = 20; 
+console.log(myClass)
+
+// re-assigning
+myClass = 30; 
+console.log(myClass)
+
+// re-declare
+var myClass = 40;
+console.log(myClass)
+
+// Difference between var let const
+// 1. Redeclare and Reassign
+//          Redeclare       Reassign
+// var =   possible        possible
+// let =   not possible    possible
+// const =  not possible    not possible
+
+// 2. Scope 
+// 3. Hoisting 
+
+
+// let kuchbhi = "Hii";                declare
+// kuchbhi = "hello"                  reassign
+// let kuchbhi = "Hello"              redeclare
+// console.log(kuchbhi)
+
+// const myNumber = 10;   
+// const myNumber = 20;  
+// myNumber = 20;        
+// console.log(myNumber)
