0e5bdda80a78478ee2adde792762b461d3817fdb
rupatul1995 <rupalichavan578@gmail.com>
2024-05-09 16:51:37 +0530

    update

---
+++ DAY-12/index.js
@@ -0,0 +1,74 @@
+// Datatypes :
+// String  - "" '' `` , template literals `My name ${name}.`
+// Number - 20 1
+// Boolean  - true false
+// Undefined
+
+// Varibles - to store data
+// Var
+// Let
+// Const
+// Redelcare and reassign
+// Scope
+// HOISTING
+
+// Conditional Statement
+// if else
+// switch
+
+// Loops
+// for
+// for(starting; ending ; sequence ){
+// }
+// while - pending
+
+// Comparision Operator
+// > < <= >= !=
+// = == ===
+// || &&
+
+// function
+// Traditional
+// Arrow
+
+// Array
+// []
+// Push pop s spl shift unshift
+// Map , foreach , reduce - pending
+
+// Nest For loop
+// for(){
+//     for(){
+//         for(){
+
+//         }
+//     }
+// }
+
+// Object
+// syntax
+// access
+// loop
+// array - object -> assign - pending
+
+
+
+
+// object -> assign 
+
+// var array=[1];
+// console.log(array[1]);
+
+
+// var array=[1];
+// array[1]=5;
+// console.log(array[1]);
+
+
+// var array=[1];
+// array[2]=5;
+// console.log(array);
+
+var myObject={name:"Awdiz"};
+myObject["Age"]=24;
+console.log(myObject);
