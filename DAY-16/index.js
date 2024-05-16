d977306e18722e10aa0b6f43cf2dbc24d1abeea2
rupatul1995 <rupalichavan578@gmail.com>
2024-05-16 16:00:17 +0530

    UPDATE

---
+++ DAY-16/index.js
@@ -0,0 +1,27 @@
+//*setTimeout(() =>{}):=>
+
+// console.log("Befor setTimeout.");
+// setTimeout(() =>{
+//     console.log("Inside setTimeout handler.");
+// },5000);
+// console.log("After setTimeout.");
+
+
+// setInterval(()=> );
+
+// setInterval(()=>{
+//     console.log("Inside set setInterval.");
+  
+// },5000);
+// console.log("After setTimeout.");
+
+
+//Event looop=>
+    console.log("Befor setTimeout.");
+setTimeout(() =>{
+    console.log("Inside setTimeout handler.");
+},5000);
+console.log("After setTimeout.");
+
+//ASYNC AWAIT:
+//PROMIS
