8c5e87ffa5304e2efbaa06b39ae874cd02b19b60
rupatul1995 <rupalichavan578@gmail.com>
2024-05-22 00:11:25 +0530

    update

--- DAY-17/index.js
+++ DAY-17/index.js
@@ -48,46 +48,46 @@
 
 // 3 time:=>
 
-    function enterData(){
-        return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-         const result=false;
-         if(result){
-            Response("data succssefully recevied");
-         }else{
-            reject("Failed");
-         }
-        })
-    })
-    }
-    enterData()
-    .then((response)=>{
-        console.log(Response,"response");
-    })
-    .catch((error)=>{
-     console.log(error,"error");
-    })
+    // function enterData(){
+    //     return new Promise((resolve,reject)=>{
+    //     setTimeout(()=>{
+    //      const result=false;
+    //      if(result){
+    //         Response("data succssefully recevied");
+    //      }else{
+    //         reject("Failed");
+    //      }
+    //     })
+    // })
+    // }
+    // enterData()
+    // .then((response)=>{
+    //     console.log(Response,"response");
+    // })
+    // .catch((error)=>{
+    //  console.log(error,"error");
+    // })
 
 
     // 4 time:=>
 
-//         function enterData(){
-//         return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//         const result=true;
-//         if(result){
-//            response("Data successfully recevied");
-//         }else{
-//           reject("Failed");
+        function enterData(){
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        const result=true;
+        if(result){
+             resolve("Data successfully recevied");
+        }else{
+            reject("Failed");
 
-//     }
-// })
-// })
-//         }
-//         enterData()
-//         .then((response)=>{
-//        console.log(response,"response");
-//         })
-//         .catch((error)=>{
-//             console.log(error,"error");
-//         })
+    }
+})
+})
+        }
+        enterData()
+        .then((response)=>{
+       console.log(response,"response");
+        })
+        .catch((error)=>{
+            console.log(error,"error");
+        })
